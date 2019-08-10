d0e4d423f3a4f3c597eddaae02af124e870d5a61
Patrick Holland <patrickeholland@gmail.com>
2019-08-10 11:59:32 -0700

    no longer need to build images before starting cluster since the images will be on docker hub

--- swarm/startCluster.sh
+++ swarm/startCluster.sh
@@ -2,8 +2,8 @@
 # 'docker swarm init' must have already been called to run this script
 
 # build the necessary docker images
-docker build -t mongo_conf ./mongo_conf_image/
-docker build -t web_app ./web_image/
+#docker build -t mongo_conf ./mongo_conf_image/
+#docker build -t web_app ./web_image/
 
 # create the docker network to be used by the swarm
 docker network create --attachable --driver overlay cluster_net
